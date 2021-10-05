4388ca523029ad2f4125c04b4d058c06a5a5b290
Meenu <meenakshi@squareup.com>
2021-10-05 10:46:14 -0700

    Use batchRetrieveOrders instead of retrieveOrder

--- functions/retrieve-order.js
+++ functions/retrieve-order.js
@@ -2,7 +2,6 @@ const config = require('./config');
 const ordersApi = config.ORDERS_API;
 
 exports.handler = function(event, context, callback) {
-  console.log("This is the ordersApi call site")
   if (event.httpMethod != 'GET') {
     return callback(null, {statusCode: 404, body: '{"error": "Not found"}'});
   }
@@ -11,9 +10,10 @@ exports.handler = function(event, context, callback) {
 
   console.log(ordersApi);
   console.log(order_id);
-//   ordersApi.retrieveOrder(order_id)
-//     .then(data =>
-//           callback(null, {statusCode: 200, body: JSON.stringify(data)}))
-//     .catch(response =>
-//            callback(null, {statusCode: 500, body: JSON.stringify(response.data)}));
+  ordersApi.batchRetrieveOrders({
+      order_ids: [order_id]
+  }).then(data =>
+          callback(null, {statusCode: 200, body: JSON.stringify(data)}))
+    .catch(response =>
+           callback(null, {statusCode: 500, body: JSON.stringify(response.data)}));
 }
